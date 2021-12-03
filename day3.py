e49fb396afaf279cc9904c6acba1fd7607db8c16
David Baker <david.baker@cambridgeconsultants.com>
2021-12-03 05:30:22 +0000

    Day 3 but I broke it

--- day3.py
+++ day3.py
@@ -1,1 +1,43 @@
+import numpy as np
+
 import common
+
+text = common.import_file('input/day3_input')
+text = common.convert_string_to_np_array(text)
+
+
+def from_binary(bool_array):
+    return np.sum(2**np.arange(len(bool_array))[::-1] * bool_array)
+
+
+gamma = from_binary(np.sum(text, axis=0) > 500)
+epsilon = from_binary(np.sum(text, axis=0) < 500)
+
+print(gamma * epsilon)
+
+
+def reduce_to_single_value(array, type='oxy'):
+    new_array = array.copy()
+    counter = 0
+    while len(new_array) > 1:
+        switch = np.sum(new_array[:, counter]) >= len(new_array) / 2
+        if type == 'oxy':
+            pass
+        elif type == 'co2':
+            switch = not switch
+
+        if switch:
+            new_array = new_array[new_array[:, counter] == 1]
+        else:
+            new_array = new_array[new_array[:, counter] == 0]
+        counter += 1
+        counter %= new_array.shape[1]
+
+    return new_array[0]
+
+
+o = reduce_to_single_value(text, 'oxy')
+c = reduce_to_single_value(text, 'co2')
+print(from_binary(o))
+print(from_binary(c))
+print(from_binary(o) * from_binary(c))
