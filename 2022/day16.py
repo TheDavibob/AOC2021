2a58fd2890e4491f726d7231e824f00979f9b355
David Baker <dibakergro@btinternet.com>
2022-12-16 18:42:02 +0000

    Finally, including actually generating all sequences...

--- 2022/day16.py
+++ 2022/day16.py
@@ -2,9 +2,20 @@ import functools
 from copy import copy
 
 import numpy as np
-from tqdm import tqdm
 
 import common
+
+
+sample_text = """Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+Valve BB has flow rate=13; tunnels lead to valves CC, AA
+Valve CC has flow rate=2; tunnels lead to valves DD, BB
+Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+Valve EE has flow rate=3; tunnels lead to valves FF, DD
+Valve FF has flow rate=0; tunnels lead to valves EE, GG
+Valve GG has flow rate=0; tunnels lead to valves FF, HH
+Valve HH has flow rate=22; tunnel leads to valve GG
+Valve II has flow rate=0; tunnels lead to valves AA, JJ
+Valve JJ has flow rate=21; tunnel leads to valve II"""
 
 
 def parse_input(text):
@@ -104,333 +115,12 @@ def convert_input(valves):
     return tuple(flow_rate), tuple(names), tuple(others), tuple(others_dist)
 
 
-def convert_input_simple(valves):
-    flow_rate = []
-    for valve in valves.values():
-        flow_rate.append(valve["flow_rate"])
-
-    others = []
-    for valve in valves.values():
-        idxs = []
-        for other in valve["others"]:
-            idx = list(valves.keys()).index(other)
-            idxs.append(idx)
-        others.append(tuple(idxs))
-
-    return tuple(flow_rate), tuple(others)
-
-
-def step(time_remaining, current_position, reduced_valve_dict):
-    if time_remaining == 0:
-        return 0
-
-    if reduced_valve_dict[current_position]["flow_rate"] == 0:
-        value_if_opened = 0
-    elif not reduced_valve_dict[current_position]["open"]:
-        next_dict = reduced_valve_dict.copy()
-        next_dict[current_position]["open"] = True
-        value_if_opened = step(
-            time_remaining-1,
-            current_position,
-            next_dict
-        ) + (time_remaining - 1) * reduced_valve_dict[current_position]["flow_rate"]
-    else:
-        value_if_opened = 0
-
-    best_movement = value_if_opened
-    for loc, dist in reduced_valve_dict[current_position]["others"].items():
-        if dist > time_remaining:
-            value_if_move = 0
-        else:
-            value_if_move = step(
-                time_remaining-dist,
-                loc,
-                reduced_valve_dict
-            )
-        if value_if_move > best_movement:
-            best_movement = value_if_move
-
-    return best_movement
-
-
-@functools.lru_cache()
-def step_2(time_remaining, current_idx, flow_rate, others, others_lengths, prev_idx):
-    if time_remaining <= 0:
-        return 0
-
-    if flow_rate[current_idx] == 0:
-        value_if_opened = 0
-    else:
-        new_flow = list(flow_rate)
-        new_flow[current_idx] = 0
-        value_if_opened = step_2(
-            time_remaining-1,
-            current_idx,
-            tuple(new_flow),
-            others,
-            others_lengths,
-            tuple()  # no previous index
-        ) + (time_remaining - 1) * flow_rate[current_idx]
-
-    if flow_rate[current_idx]*(time_remaining - 1) >= max(flow_rate)*(time_remaining-2):
-        return value_if_opened
-
-    best_movement = value_if_opened
-    for new_idx, dist in zip(others[current_idx], others_lengths[current_idx]):
-        if new_idx in prev_idx:
-            continue  # don't immediately move back
-
-        prev_idx = prev_idx + (current_idx,)
-
-        value_if_move = step_2(
-            time_remaining-dist,
-            new_idx,
-            flow_rate,
-            others,
-            others_lengths,
-            prev_idx
-        )
-        if value_if_move > best_movement:
-            best_movement = value_if_move
-
-    return best_movement
-
-
-@functools.lru_cache()
-def count_paths(
-        time_remaining,
-        current_idx,
-        flow_rate,
-        others,
-        others_lengths,
-        prev_idx
-):
-    if time_remaining <= 0:
-        return 0
-
-    n_paths = 0
-
-    if flow_rate[current_idx] != 0:
-        new_flow_rate = list(flow_rate)
-        new_flow_rate[current_idx] = 0
-        n_paths += count_paths(
-            time_remaining-1,
-            current_idx,
-            tuple(new_flow_rate),
-            others,
-            others_lengths,
-            tuple()
-        )
-
-    for new_idx, dist in zip(others[current_idx], others_lengths[current_idx]):
-        if new_idx in prev_idx:
-            continue  # don't immediately move back
-
-        prev_idx = prev_idx + (current_idx,)
-
-        n_paths += count_paths(
-            time_remaining-dist,
-            new_idx,
-            flow_rate,
-            others,
-            others_lengths,
-            prev_idx
-        )
-
-    return n_paths
-
-
-@functools.lru_cache()
-def step_elephant(time_remaining, current_idx, current_elephant, flow_rate, others,
-                  prev_idx, prev_elephant):
-    if time_remaining <= 0:
-        return 0
-
-    self_movement_options = others[current_idx]
-    elephant_movement_options = others[current_elephant]
-    if flow_rate[current_idx] != 0:
-        self_movement_options = self_movement_options + (current_idx,)
-
-    if flow_rate[current_elephant] != 0:
-        if current_elephant != current_idx:
-            elephant_movement_options = elephant_movement_options + (current_elephant,)
-
-    if flow_rate[current_idx]*(time_remaining - 1) \
-            >= max(flow_rate)*(time_remaining-2):
-        if current_idx in self_movement_options:
-            self_movement_options = (current_idx,)
-
-    if flow_rate[current_elephant]*(time_remaining - 1) \
-            >= max(flow_rate)*(time_remaining-2):
-        if current_elephant in elephant_movement_options:
-            elephant_movement_options = (current_elephant,)
-
-    self_movement_options = tuple(
-        sorted(self_movement_options, key=lambda x: flow_rate[x])[::-1]
-    )
-    elephant_movement_options = tuple(
-        sorted(elephant_movement_options, key=lambda x: flow_rate[x])[::-1]
-    )
-
-    max_gain = 0
-    for self_movement in self_movement_options:
-        if self_movement in prev_idx:
-            continue
-
-        for elephant_movement in elephant_movement_options:
-            if elephant_movement in prev_elephant:
-                continue
-
-            if (elephant_movement == self_movement) and (elephant_movement != current_elephant):
-                continue
-
-            if (elephant_movement == self_movement) and (self_movement != current_idx):
-                continue
-
-            increment = 0
-            new_flow = list(flow_rate)
-
-            if self_movement == current_idx:
-                new_flow[current_idx] = 0
-                increment += (time_remaining-1) * flow_rate[current_idx]
-                new_prev_idx = tuple()
-            else:
-                new_prev_idx = prev_idx + (current_idx,)
-
-            if elephant_movement == current_elephant:
-                new_flow[current_elephant] = 0
-                increment += (time_remaining-1) * flow_rate[current_elephant]
-                new_prev_elephant = tuple()
-            else:
-                new_prev_elephant = prev_elephant + (current_elephant,)
-
-            if elephant_movement < self_movement:
-                self_movement, elephant_movement = elephant_movement, self_movement
-                new_prev_idx, new_prev_elephant = new_prev_elephant, new_prev_idx
-
-            gain = step_elephant(
-                time_remaining-1,
-                self_movement,
-                elephant_movement,
-                tuple(new_flow),
-                others,
-                new_prev_idx,
-                new_prev_elephant
-            ) + increment
-
-            if gain > max_gain:
-                max_gain = gain
-
-            if sum(new_flow) * time_remaining - 1 < max_gain:
-                return max_gain
-
-    print(time_remaining, max_gain)
-
-    return max_gain
-
-
-def breadth_first(
-        current_time,
-        current_positions,
-        current_scores,
-        current_active_valves,
-        flow_rates,
-        links
-):
-    new_time = current_time - 1
-
-    # Should we allow "no move"?
-    new_positions = copy(current_positions)
-    new_scores = copy(current_scores)
-    new_active_valves = copy(current_active_valves)
-    # new_positions = []
-    # new_scores = []
-    # new_active_valves = []
-
-    for pos, score, valves in zip(
-            current_positions, current_scores, current_active_valves
-    ):
-        new_score = score + sum(flow_rates[v] for v in valves)
-        idx_self, idx_el = pos
-
-        opt_self = links[idx_self]
-        opt_el = links[idx_el]
-
-        if (flow_rates[idx_self] != 0) and (idx_self not in valves):
-            opt_self = opt_self + (idx_self,)
-
-        if (flow_rates[idx_el] != 0) and (idx_el not in valves):
-            if idx_el != idx_self:
-                opt_el = opt_el + (idx_el,)
-
-        opt_self = tuple(
-            sorted(opt_self, key=lambda x: flow_rates[x])[::-1]
-        )
-        opt_el = tuple(
-            sorted(opt_el, key=lambda x: flow_rates[x])[::-1]
-        )
-
-        for move_self in opt_self:
-            for move_el in opt_el:
-                new_valves = copy(valves)
-                new_position = tuple(sorted((move_self, move_el)))
-                if move_el == idx_el:
-                    new_valves.add(idx_el)
-                if move_self == idx_self:
-                    new_valves.add(idx_self)
-
-                replaced = False
-                if new_position in new_positions:
-                    # Can we improve any estimates?
-                    locs = [i for i, x in enumerate(new_positions) if x == new_position]
-                    for loc in locs:
-                        if new_valves == new_active_valves[loc]:
-                            if new_score >= new_scores[loc]:
-                                new_scores[loc] = new_score
-                                replaced = True
-                                break
-                            else:
-                                replaced = True
-                                break
-
-                        if new_valves.issubset(new_active_valves[loc]):
-                            if new_score < new_scores[loc]:
-                                # Discard - the existing is better
-                                replaced = True
-                                break
-
-                if not replaced:
-                    new_positions.append(new_position)
-                    new_scores.append(new_score)
-                    new_active_valves.append(new_valves)
-
-    unique_positions = set(new_positions)
-    new_new_positions = []
-    new_new_scores = []
-    new_new_active_valves = []
-    for unique_position in unique_positions:
-        locs_pos = [i for i, x in enumerate(new_positions) if x == unique_position]
-        unique_position_valves = (new_active_valves[loc] for loc in locs_pos)
-        for unique_position_valve in unique_position_valves:
-            unique_score = max([
-                z for x, y, z in zip(new_positions, new_active_valves, new_scores)
-                if (x == unique_position) and (y == unique_position_valve)
-            ])
-
-            new_new_positions.append(unique_position)
-            new_new_active_valves.append(set(unique_position_valve))
-            new_new_scores.append(unique_score)
-
-    return new_time, new_new_positions, new_new_scores, new_new_active_valves
-
-
 def point_to_point_distances(links, link_lengths):
     p2p_distance = np.zeros((len(links),)*2, dtype=int)
     for i, link in enumerate(links):
         for to, length in zip(link, link_lengths[i]):
             p2p_distance[i, to] = length
 
-    # let's assume it's a tree
     changed = True
     while changed:
         changed = False
@@ -442,42 +132,27 @@ def point_to_point_distances(links, link_lengths):
                 for k in range(p2p_distance.shape[0]):
                     if (p2p_distance[i, k] > 0) and (p2p_distance[k, j] > 0):
                         distance = p2p_distance[i, k] + p2p_distance[k, j]
-                        if (p2p_distance[i,j] == 0) or (distance < p2p_distance[i, j]):
+                        if (p2p_distance[i, j] == 0) or (distance < p2p_distance[i, j]):
                             p2p_distance[i, j] = distance
                             changed = True
 
     return p2p_distance
 
 
-
-sample_text = """Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
-Valve BB has flow rate=13; tunnels lead to valves CC, AA
-Valve CC has flow rate=2; tunnels lead to valves DD, BB
-Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
-Valve EE has flow rate=3; tunnels lead to valves FF, DD
-Valve FF has flow rate=0; tunnels lead to valves EE, GG
-Valve GG has flow rate=0; tunnels lead to valves FF, HH
-Valve HH has flow rate=22; tunnel leads to valve GG
-Valve II has flow rate=0; tunnels lead to valves AA, JJ
-Valve JJ has flow rate=21; tunnel leads to valve II"""
-
-
 def get_all_sequences(p2p_distances, seen_so_far, max_length, current_length):
     n_nodes = p2p_distances.shape[0]
     current_point = seen_so_far[-1]
     not_seen = [x for x in range(n_nodes) if x not in seen_so_far]
-    new_sequences = []
+    new_sequences = [seen_so_far]
     for new in not_seen:
         new_length = current_length + p2p_distances[current_point, new]
-        if new_length > max_length:
-            return [seen_so_far]
-        else:
-            new_sequences = new_sequences + (get_all_sequences(
+        if new_length <= max_length:
+            new_sequences = new_sequences + get_all_sequences(
                 p2p_distances,
                 seen_so_far + [new],
                 max_length,
                 new_length
-            ))
+            )
 
     return new_sequences
 
@@ -489,14 +164,52 @@ def get_sequence_value(sequence, p2p_distances, flow_rates, max_time):
         time += p2p_distances[fro, to] + 1
         rem_time = max_time - time
         sum_flow += rem_time * flow_rates[to]
-        print(time, sum_flow)
+        # print(time, sum_flow)
+
+    return sum_flow
+
+
+def double_value(sequence_0, sequence_1, p2p_distances, flow_rates, max_time):
+    sum_flow = 0
+    time = 0
+    idx_0 = 0
+    time_0 = 0
+    idx_1 = 0
+    time_1 = 0
+
+    while time < max_time:
+        if time_0 == time:
+            if idx_0 + 1 == len(sequence_0):
+                time_0 = max_time
+            else:
+                time_0 += p2p_distances[sequence_0[idx_0], sequence_0[idx_0+1]]
+                idx_0 = idx_0 + 1
+                time_0 += 1
+                if sequence_0[idx_0] not in sequence_1[:idx_1+1]:
+                    rem_time = max_time - time_0
+                    sum_flow += rem_time * flow_rates[sequence_0[idx_0]]
+                    # print(f"@self {sequence_0[idx_0]} {time_0}: {sum_flow}")
+
+        if time_1 == time:
+            if idx_1 + 1 == len(sequence_1):
+                time_1 = max_time
+            else:
+                time_1 += p2p_distances[sequence_1[idx_1], sequence_1[idx_1+1]]
+                idx_1 = idx_1 + 1
+                time_1 += 1
+                if sequence_1[idx_1] not in sequence_0[:idx_0+1]:
+                    rem_time = max_time - time_1
+                    sum_flow += rem_time * flow_rates[sequence_1[idx_1]]
+                    # print(f"@elephant {sequence_1[idx_1]} {time_1}: {sum_flow}")
+
+        time += 1
 
     return sum_flow
 
 
 if __name__ == "__main__":
     text = common.load_todays_input(__file__)
-    valves = parse_and_reduce_input(sample_text)
+    valves = parse_and_reduce_input(text)
     flow_rates, names, links, link_lengths = convert_input(valves)
 
     p2p = point_to_point_distances(links, link_lengths)
@@ -510,3 +223,23 @@ if __name__ == "__main__":
             max_flow = sum_flow
 
     common.part(1, max_flow)
+
+    sequences = get_all_sequences(p2p, [idx_AA], 26, 0)
+    base_seq = []
+    for sequence in sequences:
+        base_seq.append(get_sequence_value(sequence, p2p, flow_rates, 26))
+
+    sorted_sequences = [x for y, x in sorted(zip(base_seq, sequences))][::-1]
+    sorted_base = [y for y, x in sorted(zip(base_seq, sequences))][::-1]
+
+    max_flow_2 = 0
+    for i, sequence_0 in enumerate(sorted_sequences):
+        for j, sequence_1 in enumerate(sorted_sequences):
+            if sorted_base[i] + sorted_base[j] < max_flow_2:
+                break
+
+            sum_flow = double_value(sequence_0, sequence_1, p2p, flow_rates, 26)
+            if sum_flow > max_flow_2:
+                max_flow_2 = sum_flow
+
+    common.part(2, max_flow_2)
