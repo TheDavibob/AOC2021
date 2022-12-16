b7211a21cb148b52f201aaa1e905b0f7f4f7b367
David Baker <dibakergro@btinternet.com>
2022-12-16 17:04:28 +0000

    Generate all sequences?

--- 2022/day16.py
+++ 2022/day16.py
@@ -84,7 +84,9 @@ def parse_and_reduce_input(text):
 
 def convert_input(valves):
     flow_rate = []
-    for valve in valves.values():
+    names = []
+    for name, valve in valves.items():
+        names.append(name)
         flow_rate.append(valve["flow_rate"])
 
     others = []
@@ -99,7 +101,7 @@ def convert_input(valves):
         others.append(tuple(idxs))
         others_dist.append(tuple(dists))
 
-    return tuple(flow_rate), tuple(others), tuple(others_dist)
+    return tuple(flow_rate), tuple(names), tuple(others), tuple(others_dist)
 
 
 def convert_input_simple(valves):
@@ -422,12 +424,30 @@ def breadth_first(
     return new_time, new_new_positions, new_new_scores, new_new_active_valves
 
 
-def array_version(flow_rates, links):
-    n_non_zero_flows = sum(flow_rates > 0)
-    all_state_array = np.zeros(
-        (len(flow_rates), len(flow_rates), n_non_zero_flows),
-        dtype=int
-    )
+def point_to_point_distances(links, link_lengths):
+    p2p_distance = np.zeros((len(links),)*2, dtype=int)
+    for i, link in enumerate(links):
+        for to, length in zip(link, link_lengths[i]):
+            p2p_distance[i, to] = length
+
+    # let's assume it's a tree
+    changed = True
+    while changed:
+        changed = False
+        for i in range(p2p_distance.shape[0]):
+            for j in range(p2p_distance.shape[1]):
+                if i == j:
+                    continue
+
+                for k in range(p2p_distance.shape[0]):
+                    if (p2p_distance[i, k] > 0) and (p2p_distance[k, j] > 0):
+                        distance = p2p_distance[i, k] + p2p_distance[k, j]
+                        if (p2p_distance[i,j] == 0) or (distance < p2p_distance[i, j]):
+                            p2p_distance[i, j] = distance
+                            changed = True
+
+    return p2p_distance
+
 
 
 sample_text = """Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
@@ -442,34 +462,51 @@ Valve II has flow rate=0; tunnels lead to valves AA, JJ
 Valve JJ has flow rate=21; tunnel leads to valve II"""
 
 
+def get_all_sequences(p2p_distances, seen_so_far, max_length, current_length):
+    n_nodes = p2p_distances.shape[0]
+    current_point = seen_so_far[-1]
+    not_seen = [x for x in range(n_nodes) if x not in seen_so_far]
+    new_sequences = []
+    for new in not_seen:
+        new_length = current_length + p2p_distances[current_point, new]
+        if new_length > max_length:
+            return [seen_so_far]
+        else:
+            new_sequences = new_sequences + (get_all_sequences(
+                p2p_distances,
+                seen_so_far + [new],
+                max_length,
+                new_length
+            ))
+
+    return new_sequences
+
+
+def get_sequence_value(sequence, p2p_distances, flow_rates, max_time):
+    sum_flow = 0
+    time = 0
+    for fro, to in zip(sequence[:-1], sequence[1:]):
+        time += p2p_distances[fro, to] + 1
+        rem_time = max_time - time
+        sum_flow += rem_time * flow_rates[to]
+        print(time, sum_flow)
+
+    return sum_flow
+
+
 if __name__ == "__main__":
     text = common.load_todays_input(__file__)
-    valves = parse_and_reduce_input(text)
-    flow_rates, links, link_lengths = convert_input(valves)
+    valves = parse_and_reduce_input(sample_text)
+    flow_rates, names, links, link_lengths = convert_input(valves)
 
-    # best_movement = step(30, "AA", valves)
+    p2p = point_to_point_distances(links, link_lengths)
     idx_AA = list(valves.keys()).index("AA")
-    # best_movement = step_2(30, idx_AA, flow_rates, links, link_lengths, tuple())
-    # common.part(1, best_movement)
+    sequences = get_all_sequences(p2p, [idx_AA], 30, 0)
 
-    # valves = parse_input(text)
-    # flow_rates, links = convert_input_simple(valves)
-    # idx_AA = list(valves.keys()).index("AA")
-    # time = 26
-    # pos = [(idx_AA, idx_AA)]
-    # scores = [0]
-    # active = [set()]
+    max_flow = 0
+    for sequence in sequences:
+        sum_flow = get_sequence_value(sequence, p2p, flow_rates, 30)
+        if sum_flow > max_flow:
+            max_flow = sum_flow
 
-    # for _ in range(26):
-    #     time, pos, scores, active = \
-    #         breadth_first(time, pos, scores, active, flow_rates, links)
-    #     print(time, len(pos))
-
-    common.part(2, count_paths(
-        26,
-        idx_AA,
-        flow_rates,
-        links,
-        link_lengths,
-        tuple()
-    ))
+    common.part(1, max_flow)
