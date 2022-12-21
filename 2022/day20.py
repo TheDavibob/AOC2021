60377e223bcca4d14ce90c82e6eab3f0606fee83
David Baker <dibakergro@btinternet.com>
2022-12-21 05:57:10 +0000

    Tidy up

--- 2022/day20.py
+++ 2022/day20.py
@@ -16,19 +16,28 @@ def parse_input(text):
     return numbers
 
 
-def sort_list(numbers: list[int], verbose=False):
-    original_list = copy(numbers)
-    for number in original_list:
-        current_index = numbers.index(number)
+def sort_list(numbers: list[int], original_list, index_array, verbose=False):
+    for index, number in enumerate(original_list):
+        # current_index = numbers.index(number)
+        current_index = index_array[index]
+
         number = numbers.pop(current_index)
         new_index = (current_index + number)
         new_index %= len(numbers)
+
+        if new_index < current_index:
+            index_array[(index_array >= new_index) & (index_array <= current_index)] += 1
+        elif new_index > current_index:
+            index_array[(index_array <= new_index) & (index_array >= current_index)] -= 1
+        index_array[index] = new_index
+
         numbers.insert(new_index, number)
         if verbose:
             print(f"Moving {number} from {current_index} to {new_index}")
-            # print(numbers)
+            print(numbers)
+            print(index_array)
 
-    return numbers
+    return numbers, index_array
 
 
 def slow_move(numbers, number_to_move):
@@ -73,9 +82,11 @@ test_input = """1
 
 if __name__ == "__main__":
     text = common.load_todays_input(__file__)
-    numbers = parse_input(text)
+    numbers = parse_input(test_input)
+    original_list = copy(numbers)
+    index_array = np.arange(len(original_list), dtype=int)
 
-    sorted_list = sort_list(numbers, verbose=True)
+    sorted_list, _ = sort_list(numbers, original_list, index_array, verbose=False)
 
     zero_index = sorted_list.index(0)
     coordinates = [
@@ -84,4 +95,16 @@ if __name__ == "__main__":
     ]
     common.part(1, sum(coordinates))
 
-    common.part(2, "TBC")
+    new_list = [x * 811589153 for x in original_list]
+    new_original = copy(new_list)
+    index_array = np.arange(len(new_original), dtype=int)
+    for _ in range(10):
+        new_list, index_array = sort_list(new_list, new_original, index_array, verbose=False)
+
+    zero_index = new_list.index(0)
+    coordinates = [
+        new_list[(zero_index + n) % len(numbers)]
+        for n in (1000, 2000, 3000)
+    ]
+
+    common.part(2, sum(coordinates))
