2d75ab313cc814ab22b3225ab5621b3732c41293
David Baker <dibakergro@btinternet.com>
2022-12-24 08:49:54 +0000

    Part 2!

--- 2022/day24.py
+++ 2022/day24.py
@@ -1,4 +1,5 @@
 import dataclasses
+import sys
 
 import numpy as np
 import common
@@ -44,22 +45,51 @@ def step_arrays(arrays):
     return new_arrays
 
 
+def step_arrays_2(arrays):
+    new_arrays = {}
+    for direction, array in arrays.items():
+        if direction == "^":
+            new_array = np.zeros_like(array)
+            new_array[1:-1, 1:-1] = np.roll(array[1:-1, 1:-1], -1, axis=0)
+        elif direction == "v":
+            new_array = np.zeros_like(array)
+            new_array[1:-1, 1:-1] = np.roll(array[1:-1, 1:-1], 1, axis=0)
+        elif direction == "<":
+            new_array = np.zeros_like(array)
+            new_array[1:-1, 1:-1] = np.roll(array[1:-1, 1:-1], -1, axis=1)
+        elif direction == ">":
+            new_array = np.zeros_like(array)
+            new_array[1:-1, 1:-1] = np.roll(array[1:-1, 1:-1], 1, axis=1)
+        elif direction == ".":
+            new_array = array
+        else:
+            raise ValueError("Unexpected direction")
+
+        new_arrays[direction] = new_array
+
+    return new_arrays
+
+
 DIR_NAMES = {
     "D": np.array([1, 0], dtype=int),
     "R": np.array([0, 1], dtype=int),
-    "W": np.array([0, 0], dtype=int),
     "U": np.array([-1, 0], dtype=int),
     "L": np.array([0, -1], dtype=int),
+    "W": np.array([0, 0], dtype=int),
 }
 
 
-def all_moves(current_position, next_blizzard):
+def all_moves(current_position, next_blizzard, pref_order=None):
+    if pref_order is None:
+        pref_order = "DRULW"
+
     options = []
 
     blizzard_map = np.sum([dir_map for dir_map in next_blizzard.values()], axis=0)
     blizzard_shape = np.array(next_blizzard[">"].shape)
 
-    for option, displacement in DIR_NAMES.items():
+    for option in pref_order:
+        displacement = DIR_NAMES[option]
         proposed_position = current_position + displacement
 
         if any(proposed_position < 0):
@@ -135,12 +165,84 @@ def from_start(arrays, entry_exit):
     return len_to_exit(start_postition, end_position, arrays, 0, best_so_far, Cache(lookup={}))
 
 
+def len_to_exit_2(current_position, targets, arrays, current_distance, best_so_far, cache, pref_orders):
+    manhattan_remaining = np.sum(np.abs(current_position - targets[0]))
+    for t0, t1 in zip(targets[:-1], targets[1:]):
+        manhattan_remaining += np.sum(np.abs(t0 - t1))
+
+    if (current_distance + manhattan_remaining) >= best_so_far.value:
+        return -1
+
+    cache_state = (tuple(current_position), current_distance, len(targets))
+    if (cached := cache.lookup.get(cache_state, None)) is not None:
+        return cached
+
+    if np.all(current_position == targets[0]):
+        print(current_distance, current_position, len(targets), best_so_far.value)
+        new_targets = targets[1:]
+        pref_orders = pref_orders[1:]
+        if not new_targets:
+            if current_distance < best_so_far.value:
+                best_so_far.value = current_distance
+            return 0
+    else:
+        new_targets = targets
+
+    next_blizzard = step_arrays_2(arrays)
+    options = all_moves(current_position, next_blizzard, pref_orders[0])
+    if len(options) == 0:
+        return -1
+
+    best_distance = -1
+    for option in options:
+        new_position = current_position + DIR_NAMES[option]
+        distance = len_to_exit_2(new_position, new_targets, next_blizzard, current_distance + 1, best_so_far, cache, pref_orders)
+        if distance > -1:
+            if best_distance == -1:
+                best_distance = distance + 1
+            if distance + 1 < best_distance:
+                best_distance = distance + 1
+
+    cache.lookup[cache_state] = best_distance
+
+    return best_distance
+
+
+def from_start_to_end_to_start(arrays, entry_exit):
+    start_postition = np.array(entry_exit[0], dtype=int) + 1
+    end_position = np.array(entry_exit[1], dtype=int) + 1
+    for dir, array in arrays.items():
+        new_array = np.zeros((array.shape[0]+2, array.shape[1]+2))
+        new_array[1:-1, 1:-1] = array
+        arrays[dir] = new_array
+
+    wall_array = np.ones(new_array.shape)
+    wall_array[1:-1, 1:-1] = 0
+    wall_array[tuple(start_postition)] = 0
+    wall_array[tuple(end_position)] = 0
+    arrays["."] = wall_array
+
+    best_so_far = Pointer(value=1500)
+
+    return len_to_exit_2(
+        start_postition,
+        [end_position, start_postition, end_position],
+        arrays,
+        0,
+        best_so_far,
+        Cache(lookup={}),
+        ["DRLWU", "ULRWD", "DRLWU"]
+    )
+
+
 if __name__ == "__main__":
     text = common.load_todays_input(__file__)
     arrays, entry_exit = parse_input(text)
 
-    distance = from_start(arrays, entry_exit)
+    # distance = from_start(arrays, entry_exit)
+    #
+    # common.part(1, distance)
 
-    common.part(1, "TBC")
-
-    common.part(2, "TBC")
+    sys.setrecursionlimit(1600)
+    distance = from_start_to_end_to_start(arrays, entry_exit)
+    common.part(2, distance)
